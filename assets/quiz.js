5f6fa4112c15e4508da5dd36a56bf306ba166ec6
Ashley <ashleyh5440@gmail.com>
2023-10-16 15:32:08 -0400

    tried to fix timer

--- assets/quiz.js
+++ assets/quiz.js
@@ -9,123 +9,129 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+let timeInterval;
 
 // timer
-// document.getElementById("timer").innerHTML = 05 + ":" + 00; //starts timer at 5 minutes
-// startTimer();
+document.getElementById("timer").innerHTML = 05 + ":" + 00; //starts timer at 5 minutes
+startTimer();
 
-// function startTimer () {
-//     let presentTime = document.getElementById("timer").innerHTML;
-//     let timeArray = presentTime.split(/[:]+/);
-//     let m = timeArray[0];
-//     let s = checkSecond((timeArray[1] - 1));
-//     if(s==59){m=m-1}
+function startTimer () {
+    let presentTime = document.getElementById("timer").innerHTML;
+    let timeArray = presentTime.split(/[:]+/);
+    let m = timeArray[0];
+    let s = checkSecond((timeArray[1] - 1));
+    if(s == 59){m = m - 1}
+    // if(m < 1 && s == 0 )
 
-//     document.getElementById("timer").innerHTML = m + ":" + s;
-//     setTimeout(startTimer, 1000) //makes seconds count down by the second
-// }
+    document.getElementById("timer").innerHTML = m + ":" + s;
+    // clearTimeout(timeInterval)
+    timeInterval = setTimeout(startTimer, 1000) //makes seconds count down by the second
+    // has to clear if they run out of time, questions, or lose time
 
-// function checkSecond(sec) {
-//     if (sec < 10 && sec >= 0) { sec = "0" + sec}; //adds 0 to numbers less than 10
-//     if (sec < 0) {sec = "59";}
-//     return sec;
-// }
+}
+
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) { sec = "0" + sec}; //adds 0 to numbers less than 10
+    if (sec < 0) {sec = "59";}
+    return sec;
+}
 
 //need a function to end the timer and bring users to the score
 // ^ need to use clearInterval method somehow
 
+// const question = document.querySelector("#question");
+// const choice = Array.from(document.querySelectorAll (".choice"));
 
+// // quiz questions
+// let questions = [
+//     {
+//     number: 1,
+//     question: "How are variables declared?",
+//     answer: "var, let, const",
+//     options: [
+//         "href, var, src",
+//         "if, function, const",
+//         "var, let, const"
+//             ]
+//     }, 
 
-// quiz questions
-let questions = [
-    {
-    number: 1,
-    question: "How are variables declared?",
-    answer: "var, let, const",
-    options: [
-        "href, var, src",
-        "if, function, const",
-        "var, let, const"
-            ]
-    }, 
+//     {
+//     number: 2, 
+//     question: "What is the differenence between function declarations and function expressions?",
+//     answer: "Function declarations are global and load before any code is executed; function expressions are local and only load when that line is reached"
+//     options: [
+//         "They're the same",
+//         "Function declarations are global and load before any code is executed; function expressions are local and only load when that line is reached",
+//         "Function declarations are local and only load when that line is reached; function expressions are global and load before any code is executed"
+//             ]
+//     },
 
-    {
-    number: 2, 
-    question: "What is the differenence between function declarations and function expressions?",
-    answer: "Function declarations are global and load before any code is executed; function expressions are local and only load when that line is reached"
-    options: [
-        "They're the same",
-        "Function declarations are global and load before any code is executed; function expressions are local and only load when that line is reached",
-        "Function declarations are local and only load when that line is reached; function expressions are global and load before any code is executed"
-            ]
-    },
+//     {
+//     number: 3,
+//     question: "What are the different types of variables?",
+//     answer: "undefined, string, number, boolean",
+//     options: [
+//         "undefined, string, number, boolean",
+//         "tag, string, number, stylesheet",
+//         "boolean, method, number, function"
+//             ]
+//     },
 
-    {
-    number: 3,
-    question: "What are the different types of variables?",
-    answer: "undefined, string, number, boolean",
-    options: [
-        "undefined, string, number, boolean",
-        "tag, string, number, stylesheet",
-        "boolean, method, number, function"
-            ]
-    },
+//     {
+//     number: 4,
+//     question: "How do you add a comment in JavaScript?"
+//     answer: "// or /* */"
+//     options: [
+//         "<!-- --> or //",
+//         "$$ or @@",
+//         "// or /* */"
+//             ]
+//     },
 
-    {
-    number: 4,
-    question: "How do you add a comment in JavaScript?"
-    answer: "// or /* */"
-    options: [
-        "<!-- --> or //",
-        "$$ or @@",
-        "// or /* */"
-            ]
-    },
+//     {
+//     number: 5,
+//     question: "What is the correct format for an array?",
+//     answer: "var colors = ['black', 'orange', 'green']", 
+//     options: [
+//         "var colors = ['black', 'orange', 'green']", 
+//         "var colors = black orange green",
+//         "var colors = (black), (orange), (green)"
+//             ]
+//     },
 
-    {
-    number: 5,
-    question: "What is the correct format for an array?",
-    answer: "var colors = ['black', 'orange', 'green']", 
-    options: [
-        "var colors = ['black', 'orange', 'green']", 
-        "var colors = black orange green",
-        "var colors = (black), (orange), (green)"
-            ]
-    },
+//     {
+//     number: 6,
+//     question: "Where do you link a JavaScript file in an HTML document?",
+//     answer: "At the bottom of the body with <script> tags",
+//     options: [
+//         "At the top of the body in a <link> tag",
+//         "At the bottom of the body with <script> tags",
+//         "In the middle of the body with <a> tags"
+//             ]
+//     },
 
-    {
-    number: 6,
-    question: "Where do you link a JavaScript file in an HTML document?",
-    answer: "At the bottom of the body with <script> tags",
-    options: [
-        "At the top of the body in a <link> tag",
-        "At the bottom of the body with <script> tags",
-        "In the middle of the body with <a> tags"
-            ]
-    },
+//     {
+//     number: 7,
+//     question: "How does a for loop start?",
+//     answer: "for (let i = 0;",
+//     options: [
+//         "for (let i = 0;",
+//         "for let (i = 0;",
+//         "for let (i) = (0"
+//     ]
+//     },
 
-    {
-    number: 7,
-    question: "How does a for loop start?",
-    answer: "for (let i = 0;",
-    options: [
-        "for (let i = 0;",
-        "for let (i = 0;",
-        "for let (i) = (0"
-    ]
-    },
-
-    {
-    number: 8,
-    question: "How do assign value to a variable?"
-    answer: "=",
-    options: [
-        "+",
-        "%",
-        "="
-    ]
-    }
-];
+//     {
+//     number: 8,
+//     question: "How do assign value to a variable?"
+//     answer: "=",
+//     options: [
+//         "+",
+//         "%",
+//         "="
+//     ]
+//     }
+// ];
 
 /* need function for next question button
 need to keep track of scores
